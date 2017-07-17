a7b705ecc38139e4513b5d3b6bd54625eed02384
Fortunella007 <svyatoslav.kotov.1997.ru@gmail.com>
2017-07-17 23:24:04 +0300

    Добавил точку с запятой

--- Test/СSharpProj/СSharpProj/Program.cs
+++ Test/СSharpProj/СSharpProj/Program.cs
@@ -10,7 +10,7 @@ namespace СSharpProj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world;");
         }
     }
 }
